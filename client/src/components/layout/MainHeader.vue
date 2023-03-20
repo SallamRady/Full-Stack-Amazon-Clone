577f2dcff7e35e8a9c5085dd0a62d1a2aed2594a
SallamRady <sallamrady@gmail.com>
2023-03-20 14:24:21 +0200

    setup front-end

---
+++ client/src/components/layout/MainHeader.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="main-header">
+    <div class="right">
+      <img
+        src="../../assets/Amazon-Logo-ligt.png"
+        class="logo"
+        width="70"
+        height="40"
+        alt="Logo"
+      />
+      <div class="location">
+        <i class="bi bi-geo-alt" />
+        <div class="action-options">
+          <span>Hello</span><br />
+          <span><b>Select Your Address</b></span>
+        </div>
+      </div>
+    </div>
+    <div class="middle">
+      <form>
+        <select>
+          <option>All</option>
+          <option>Mobiles</option>
+          <option>Computers</option>
+        </select>
+        <input type="search" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+    <div class="left">
+      <div>
+        <span> Hello Guest </span><br />
+        <span><b>Sign In</b></span>
+      </div>
+      <div>
+        <span>Return</span><br />
+        <span><b>Orders</b></span>
+      </div>
+      <div>
+        <span>Your</span><br />
+        <span><b>Prime</b></span>
+      </div>
+      <div>Cart</div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+div.main-header {
+  background-color: #131921;
+  min-height: 89px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+div.main-header > div {
+  width: 33%;
+  color: #fff;
+  display: flex;
+  justify-content: space-around;
+}
+
+div.main-header .middle form * {
+  min-height: 30px;
+}
+div.main-header .middle form input {
+  width: 15rem;
+}
+
+div.main-header .middle form *:focus {
+  border: 1.88px solid rgb(25, 186, 240);
+  outline: none;
+}
+
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fab);
+import { far } from "@fortawesome/free-regular-svg-icons";
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
