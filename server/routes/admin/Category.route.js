da69898523c6faf3639d64bee58de7ddfe932c99
SallamRady <sallamrady@gmail.com>
2023-03-22 11:36:58 +0200

    Add validation layer on backend

--- server/routes/admin/Category.route.js
+++ server/routes/admin/Category.route.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const check = require("express-validator").check;
 const categoryController = require("../../controllers/admin/Category.controller");
 
 // route : /admin/create/category - create new Category
-router.post("/create/category", categoryController.createCategory);
+router.post(
+  "/create/category",
+  /* validate incomming data */
+  check("title").notEmpty().withMessage("Title is required!"),
+  /*    handle request method   */
+  categoryController.createCategory
+);
 
 // route : /admin/categories - create new Category
 router.get("/categories", categoryController.getCategories);
 
 // route : /admin/edit/category - edit Category
-router.put("/edit/category", categoryController.updateCategory);
+router.put(
+  "/edit/category",
+  /* validate incomming data */
+  check("id").exists().notEmpty().withMessage("id is required!"),
+  check("title").notEmpty().withMessage("Title is required!"),
+  /*    handle request method   */
+  categoryController.updateCategory
+);
 
 // route : /admin/delete/category - delete Category
-router.delete("/delete/category", categoryController.deleteCategory);
+router.delete(
+  "/delete/category",
+  /* validate incomming data */
+  check("id").exists().notEmpty().withMessage("id is required!"),
+  /*    handle request method   */
+  categoryController.deleteCategory
+);
 
 module.exports = router;
--- server/routes/admin/product.route.js
+++ server/routes/admin/product.route.js
@@ -1,17 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const check = require("express-validator").check;
 const productController = require("../../controllers/admin/Product.controller");
 
 // route : /admin/create/product - create new Product
-router.post("/create/product",productController.createProduct);
+router.post(
+  "/create/product",
+  /* validate incomming data */
+  check("title").notEmpty().withMessage("Title is required!"),
+  check("description").notEmpty().withMessage("Description is required!"),
+  check("stockQuantity").isNumeric().withMessage("Value must be Numeric"),
+  check("price").isNumeric().withMessage("Value must be Numeric"),
+  check("ownerID").exists().withMessage("ownerID is required"),
+  check("categoryID").exists().withMessage("categoryID is required"),
+  /*    handle request method   */
+  productController.createProduct
+);
 
 // route : /admin/products - fetch Products
-router.get("/products",productController.getProducts);
+router.get("/products", productController.getProducts);
 
 // route : /admin/edit/product - edit Product
-router.put("/edit/product",productController.updateProduct);
+router.put(
+  "/edit/product",
+  /* validate incomming data */
+  check("id").exists().notEmpty().withMessage("Id is required"),
+  check("title").notEmpty().withMessage("Title is required!"),
+  check("description").notEmpty().withMessage("Description is required!"),
+  check("stockQuantity").isNumeric().withMessage("Value must be Numeric"),
+  check("price").isNumeric().withMessage("Value must be Numeric"),
+  check("ownerID").exists().withMessage("ownerID is required"),
+  check("categoryID").exists().withMessage("categoryID is required"),
+  /*    handle request method   */
+  productController.updateProduct
+);
 
 // route : /admin/delete/product - delete Product
-router.delete("/delete/product",productController.deleteProduct);
+router.delete(
+  "/delete/product",
+  /* validate incomming data */
+  check("id").exists().notEmpty().withMessage("Id is required"),
+  productController.deleteProduct
+);
 
 module.exports = router;
